0fa80a7e2861d42afc25c3f64e1961694282dcf1
timaeusTestified-heart <miras.arbuz88@gmail.com>
2026-02-16 00:24:12 +0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
-app.use(express.static("public")); // отдаём всё из public
+app.use(express.static("public")); // отдаём index.html и скрипты
 
 let users = [];
 
@@ -35,22 +35,6 @@ io.on("connection", socket => {
     updateUserList();
   });
 
-  function updateUserList() {
-    const list = users.map(u => ({name:u.nickname,color:u.color,mood:u.mood}));
-    users.forEach(u => u.emit("user list", list));
-  }
-
-});
-
-http.listen(process.env.PORT || 3000, () => console.log("server running"));
-  function updateUserList() {
-    const list = users.map(u => ({name:u.nickname,color:u.color,mood:u.mood}));
-    users.forEach(u => u.emit("user list", list));
-  }
-
-});
-
-http.listen(process.env.PORT || 3000, () => console.log("server running"));
   function updateUserList() {
     const list = users.map(u => ({name:u.nickname,color:u.color,mood:u.mood}));
     users.forEach(u => u.emit("user list", list));
