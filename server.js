77b1bd01a826d4bf73655186eb961084576d631b
timaeusTestified-heart <miras.arbuz88@gmail.com>
2026-02-15 21:28:59 +0500

    server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+http.listen(3000, () => {
+  console.log("server started");
+});
