59da1da0071014730212209ab3db9316028c30a6
timaeusTestified-heart <miras.arbuz88@gmail.com>
2026-02-16 01:07:29 +0500

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
-app.use(express.static("public")); // отдаём index.html и скрипты
+app.use(express.static("public")); // отдаём index.html и все файлы из public/
 
-let users = [];
+let users = []; // список подключенных пользователей
 
-io.on("connection", socket => {
+io.on("connection", (socket) => {
 
-  socket.on("join", data => {
+  // когда клиент присоединяется
+  socket.on("join", (data) => {
     socket.nickname = data.name;
     socket.color = data.color;
     socket.mood = data.mood;
@@ -17,29 +18,35 @@ io.on("connection", socket => {
     updateUserList();
   });
 
-  socket.on("private message", msg => {
-    const toSocket = users.find(u => u.nickname === msg.to);
-    if(toSocket) {
-      toSocket.emit("private message", msg);
-      socket.emit("private message", msg);
-    }
+  // системные сообщения (общий чат)
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg); // всем подключенным
   });
 
-  socket.on("update mood", data => {
+  // приватные сообщения
+  socket.on("private message", (msg) => {
+    const toSocket = users.find(u => u.nickname === msg.to);
+    if(toSocket) toSocket.emit("private message", msg);
+    socket.emit("private message", msg); // отправляем себе тоже
+  });
+
+  // обновление настроения
+  socket.on("update mood", (data) => {
     socket.mood = data.mood;
     updateUserList();
   });
 
+  // отключение пользователя
   socket.on("disconnect", () => {
     users = users.filter(u => u !== socket);
     updateUserList();
   });
 
   function updateUserList() {
-    const list = users.map(u => ({name:u.nickname,color:u.color,mood:u.mood}));
+    const list = users.map(u => ({name:u.nickname, color:u.color, mood:u.mood}));
     users.forEach(u => u.emit("user list", list));
   }
-
 });
 
+// порт Render или 3000 локально
 http.listen(process.env.PORT || 3000, () => console.log("server running"));
