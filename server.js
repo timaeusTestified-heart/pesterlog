5ebb45ef59925f3f8d1e8871eed70ee80bdcc957
timaeusTestified-heart <miras.arbuz88@gmail.com>
2026-02-16 14:27:37 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,68 +1,55 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const http = require("http").createServer(app);
-const { Server } = require("socket.io");
-
-const io = new Server(http, {
-  cors: {
-    origin: "*"
-  }
-});
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 const PORT = process.env.PORT || 3000;
 
-// обязательно для render
-app.use(express.static("public"));
+app.use(express.static('public')); // все файлы в папке public
 
+// хранение информации о пользователях
 let users = {};
 
-// подключение
-io.on("connection", (socket) => {
-
-  // вход
-  socket.on("join", (data) => {
+io.on('connection', (socket) => {
+  // пользователь заходит
+  socket.on('join', (data) => {
     users[socket.id] = {
       name: data.name,
       color: data.color,
       mood: data.mood
     };
-
-    io.emit("user list", Object.values(users));
+    io.emit('user list', Object.values(users));
   });
 
-  // общий чат
-  socket.on("chat message", (data) => {
-    io.emit("chat message", data);
+  // системный чат
+  socket.on('chat message', (data) => {
+    io.emit('chat message', data);
   });
 
-  // приватные сообщения
-  socket.on("private message", (data) => {
-    const targetId = Object.keys(users).find(
-      id => users[id].name === data.to
-    );
-
-    if (targetId) {
-      io.to(targetId).emit("private message", data);
-      socket.emit("private message", data);
+  // приватный чат
+  socket.on('private message', (data) => {
+    for (let id in users) {
+      if (users[id].name === data.to || users[id].name === data.from) {
+        io.to(id).emit('private message', data);
+      }
     }
   });
 
   // обновление настроения
-  socket.on("update mood", (data) => {
+  socket.on('update mood', (data) => {
     if (users[socket.id]) {
       users[socket.id].mood = data.mood;
-      io.emit("user list", Object.values(users));
+      io.emit('user list', Object.values(users));
     }
   });
 
-  // отключение
-  socket.on("disconnect", () => {
+  // выход пользователя
+  socket.on('disconnect', () => {
     delete users[socket.id];
-    io.emit("user list", Object.values(users));
+    io.emit('user list', Object.values(users));
   });
-
 });
 
 http.listen(PORT, () => {
-  console.log("server running on port " + PORT);
+  console.log(`Server running on port ${PORT}`);
 });
